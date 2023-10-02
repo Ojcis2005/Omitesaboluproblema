20726683747574e147662a3121ad36b9494ee90c
Ojcis2005 <112924730+Ojcis2005@users.noreply.github.com>
2023-10-02 19:31:29 +0300

    gdfhdfh

--- gui.py
+++ gui.py
@@ -1,15 +1,67 @@
 import PySimpleGUI as sg
 
-layout = [
-    [sg.B("Auglis", button_color="red")],
-    [sg.B("Dārzenis", button_color="orange")]
-]
+def create_layout(products):
+    layout = [
+        [sg.Text('Lauku labuma nosaukums:'), sg.InputText(key='product_name')],
+        [sg.Text('Daudzums'), sg.InputText(key='product_quantity')],
+        [sg.Button('Pievienot labumu'), sg.Button('Izmantot labumu'), sg.Button('Izveidot ievārjijumu'), sg.Button('Rediģēt labumu')],
+        [sg.Text('Labumu Saraksts:')],
+        [sg.Listbox(values=[str(product) for product in products], size=(40, 6), key='product_list')],
+        [sg.Button('Iziet')],
+    ]
+    return layout
 
-window = sg.Window("My app",layout)
-event, values = window.read()
-if event == "Auglis":
-    sg.popup("Kāds Auglis?")
-elif event == "Dārzenis":
-    sg.popup("Kāds Dārzenis?")
+def main():
+    products = []
+    window = sg.Window('Produkta Izsekotājs', create_layout(products))
 
-print(event, values)
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == 'Iziet':
+            break
+
+        if event == 'Pievienot labumu':
+            name = values['product_name']
+            quantity = float(values['product_quantity']) if values['product_quantity'] else 0.0
+            products.append(products(name, quantity))
+            window['product_list'].update(products)
+
+        if event == 'Consume Product':
+            selected_product = window['product_list'].get()
+            if selected_product:
+                product = selected_product[0]
+                name = product.name
+                quantity = float(values['product_quantity']) if values['product_quantity'] else 0.0
+                for p in products:
+                    if p.name == name:
+                        p.consume(quantity)
+                window['product_list'].update(products)
+
+        if event == 'Izveidot ievārjijumu':
+            selected_product = window['product_list'].get()
+            if selected_product:
+                product = selected_product[0]
+                name = product.name
+                quantity = float(values['product_quantity']) if values['product_quantity'] else 0.0
+                for p in products:
+                    if p.name == name:
+                        jam_product = p.make_jam(quantity)
+                        if jam_product:
+                            products.append(jam_product)
+                window['product_list'].update(products)
+
+        if event == 'Rediģēt labumu':
+            selected_product = window['product_list'].get()
+            if selected_product:
+                product = selected_product[0]
+                name = product.name
+                new_name = values['product_name']
+                if new_name:
+                    product.name = new_name
+                window['product_list'].update(products)
+
+    window.close()
+
+if __name__ == "__main__":
+    main()
--- klases.py
+++
@@ -1,6 +0,0 @@
-class Auglis:
-  def __init__(self, number, tips, name):
-    self.tips = tips
-    self.number = number
-    self.name = name
-
