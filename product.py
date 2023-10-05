40277d734ec3d5dd8e6b1df09fd60e32cf249009
Ojcis2005 <112924730+Ojcis2005@users.noreply.github.com>
2023-10-05 20:47:08 +0300

    aa
    aa

--- product.py
+++ product.py
@@ -1,4 +1,4 @@
-class Product:
+class Product:                     #labumu klase
     def __init__(self, name, quantity=0):
         self.name = name
         self.quantity = quantity
@@ -6,11 +6,11 @@ class Product:
     def add_quantity(self, quantity):
         self.quantity += quantity
 
-    def consume(self, quantity):
+    def consume(self, quantity):         #produkta apēšana
         if self.quantity >= quantity:
             self.quantity -= quantity
         else:
-            print(f"Not enough {self.name} available for consumption.")
+            print(f"Nav pietiekami {self.name} lai apēstu.")
 
     def __str__(self):
         return f"{self.name} ({self.quantity} kg)"
